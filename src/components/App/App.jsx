e4d1f87df5587de29b1c6e54aad1292f7a9987d5
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-09-08 22:11:10 +0300

    add CatalogPage and CarDetailsPage into App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import "./App.module.css";
 import { Routes, Route } from "react-router-dom";
 import { Suspense } from "react";
 import { HomePage } from "../../pages/HomePage/HomePage";
+import { CatalogPage } from "../../pages/CatalogPage/CatalogPage";
+import { CarDetailsPage } from "../../pages/CarDetailsPage/CarDetailsPage";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Suspense>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:id" element={<CarDetailsPage />} />
         </Routes>
       </Suspense>
     </>
