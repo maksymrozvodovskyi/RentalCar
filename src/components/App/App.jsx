0a2a14eff04b6f9551eabf63105d6e83717a3d47
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-09-09 20:55:23 +0300

    delete unused imports

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,9 @@
 import "./App.module.css";
-import css from "../../styles/container.module.css";
 import { Routes, Route } from "react-router-dom";
 import { Suspense } from "react";
 import HomePage from "../../pages/HomePage/HomePage";
 import CatalogPage from "../../pages/CatalogPage/CatalogPage";
 import CarDetailsPage from "../../pages/CarDetailsPage/CarDetailsPage";
-import Header from "../Header/Header";
 
 function App() {
   return (
