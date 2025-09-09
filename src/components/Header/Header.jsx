5494aa6b2c6454cc495d7ac983af751bb1a44994
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-09-09 19:28:36 +0300

    add header markup and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,25 @@
 import Logo from "../Logo/Logo";
 import css from "./Header.module.css";
-import container from "../../styles/container.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className={css.header}>
       <Logo />
+      <nav className={css.nav}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? css.active : css.link)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/catalog"
+          className={({ isActive }) => (isActive ? css.active : css.link)}
+        >
+          Catalog
+        </NavLink>
+      </nav>
     </header>
   );
 }
