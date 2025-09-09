b26571ac7db52ba2eecd3b11c11f1537c18069c6
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-09-09 13:23:59 +0300

    add basic redux structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
