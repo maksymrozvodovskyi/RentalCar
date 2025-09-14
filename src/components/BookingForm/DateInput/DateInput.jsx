d449a7cc4cc90c15fe7b91ebdd07c16e6c2ef8c5
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-09-14 15:24:06 +0300

    add styles

--- src/components/BookingForm/DateInput/DateInput.jsx
+++ src/components/BookingForm/DateInput/DateInput.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import css from "./DateInput.module.css";
 
-export default function DateInput() {
+export default function DateInput({ onChange }) {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleChange = (date) => {
+    setSelectedDate(date);
+    if (onChange) onChange(date);
+  };
+
   return (
-    <>
-      <input
-        type="date"
-        name="date"
-        placeholder="Booking date"
-        required
-        className={css.dateInput}
-      />
-    </>
+    <DatePicker
+      selected={selectedDate}
+      onChange={handleChange}
+      placeholderText="Booking date"
+      className={css.dateInput}
+      dateFormat="dd/MM/yyyy"
+      required
+    />
   );
 }
